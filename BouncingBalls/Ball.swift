ff06c96f61243fe23fdce7fe7aa850a44b677345
Vohra <nikant@outlook.com>
2014-12-26 18:52:44 +0530

    added ball and tile class

---
+++ BouncingBalls/Ball.swift
@@ -0,0 +1,9 @@
+//
+//  Ball.swift
+//  BouncingBalls
+//
+//  Created by Vohra, Nikant on 12/26/14.
+//  Copyright (c) 2014 Vohra, Nikant. All rights reserved.
+//
+
+import Foundation
---
+++ BouncingBalls/Tile.swift
@@ -0,0 +1,9 @@
+//
+//  Tile.swift
+//  BouncingBalls
+//
+//  Created by Vohra, Nikant on 12/26/14.
+//  Copyright (c) 2014 Vohra, Nikant. All rights reserved.
+//
+
+import Foundation
